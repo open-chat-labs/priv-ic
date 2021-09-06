4db9a34a297348879bea876863b6ca23d4b4b601
megrogan <megrogan@gmail.com>
2021-09-06 11:42:49 +0100

    Implement remove_attribute

--- backend/canisters/identity/api/src/updates/mod.rs
+++ backend/canisters/identity/api/src/updates/mod.rs
@@ -2,5 +2,6 @@ pub mod confirm_verification_code;
 pub mod ext_remove_verification_codes;
 pub mod register_application;
 pub mod register_attribute;
+pub mod remove_attribute;
 pub mod send_verification_code;
 pub mod set_visible_attributes;
--- backend/canisters/identity/impl/src/model/identity.rs
+++ backend/canisters/identity/impl/src/model/identity.rs
@@ -9,6 +9,10 @@ pub struct Identity {
 }
 
 impl Identity {
+    pub fn get_mut(&mut self, id: &AttributeId) -> Option<&mut Attribute> {
+        self.attributes.get_mut(id)
+    }
+    
     pub fn values(&self) -> hash_map::Values<'_, AttributeId, Attribute> {
         self.attributes.values()
     }
@@ -17,8 +21,11 @@ impl Identity {
         self.attributes.insert(attribute.id(), attribute);
     }
 
-    pub fn get_mut(&mut self, id: &AttributeId) -> Option<&mut Attribute> {
-        self.attributes.get_mut(id)
+    pub fn remove(&mut self, attribute_id: AttributeId) -> bool {
+        match self.attributes.remove(&attribute_id) {
+            None => false,
+            Some(_) => true,
+        }
     }
 }
 
