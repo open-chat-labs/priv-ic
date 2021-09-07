4b70f6e6e2a02a7f9152d4f2a5db3258e54ddc47
Hamish Peebles <hamishpeebles@gmail.com>
2021-09-07 11:13:38 +0100

    Run verification code sender as long running process (#19)

--- backend/external/verification_code_sender/aws/src/main.rs
+++ backend/external/verification_code_sender/aws/src/main.rs
@@ -6,36 +6,18 @@ use crate::dynamodb::DynamoDbClient;
 use crate::ses::SesClient;
 use crate::sns::SnsClient;
 use candid::Principal;
-use lambda_runtime::{handler_fn, Context};
 use log::info;
-use serde::Deserialize;
 use std::str::FromStr;
 use types::Error;
-use verification_code_sender::actions::{remove_codes, send_codes};
 use verification_code_sender::ic_agent::{IcAgent, IcAgentConfig};
-
-#[derive(Deserialize)]
-struct Request {
-    run_mode: Mode,
-}
-
-#[derive(Deserialize)]
-enum Mode {
-    SendCodes,
-    RemoveCodes,
-}
+use verification_code_sender::runner;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     dotenv::dotenv()?;
     env_logger::init();
     info!("Starting...");
-    let func = handler_fn(handler);
-    lambda_runtime::run(func).await?;
-    Ok(())
-}
 
-async fn handler(request: Request, _: Context) -> Result<(), Error> {
     let canister_id = Principal::from_text(dotenv::var("NOTIFICATIONS_CANISTER_ID")?).unwrap();
     let dynamodb_client = DynamoDbClient::build(canister_id);
     let ic_url = dotenv::var("IC_URL")?;
@@ -48,15 +30,10 @@ async fn handler(request: Request, _: Context) -> Result<(), Error> {
         fetch_root_key: is_development,
     };
     let ic_agent = IcAgent::build(&ic_agent_config, canister_id).await?;
+    let sns_client = SnsClient::build()?;
+    let ses_client = SesClient::build()?;
 
     info!("Configuration complete");
 
-    match request.run_mode {
-        Mode::SendCodes => {
-            let sns_client = SnsClient::build()?;
-            let ses_client = SesClient::build()?;
-            send_codes::run(&ic_agent, &dynamodb_client, &sns_client, &ses_client).await
-        }
-        Mode::RemoveCodes => remove_codes::run(&ic_agent, &dynamodb_client).await,
-    }
+    runner::run(&ic_agent, &dynamodb_client, &sns_client, &ses_client).await
 }
